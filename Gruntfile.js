57c593b117eed17212c6930bae9a2651d373d3e7
sh4h <shahatwork19@gmail.com>
2015-09-04 02:23:31 +0530

    build setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
         uglify: {
 
             build: {
-                src: ['js/jq.js', 'js/jquery.opacityrollover.js', 'jquery.galleriffic.js' ],
+                src: ['js/jq.js', 'js/jquery.opacityrollover.js', 'js/jquery-ui.min.js', 'js/jquery.galleriffic.js', 'js/gallery.js', 'js/cb.js' ],
                 dest: 'build/js/cB_02.min.js'
             }
         },
@@ -20,15 +20,36 @@ module.exports = function(grunt) {
                     "css/styles.css": "css/style.less"
                 }
             }
-        }
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'build/css/cb_min.css': ['css/basic.css', 'css/font-icon.css', 'css/galleriffic-5.css', 'css/jquery-ui.min.css', 'css/styles.css']
+                }
+            }
+        },
+		copy: {
+		  main: {
+			files: [
+			  // includes files within path
+			  {expand: true, src: ['images/**'], dest: 'build/'},
+			  {expand: true, src: ['css/iconFonts/*'], dest: 'build/', filter: 'isFile'},
+			  {expand: true, src: ['index.html'], dest: 'build/'}
+			]
+		  }
+		}
+
+		
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
 // Load the plugin that provides the "less" task.
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'less']);
+    grunt.registerTask('default', ['uglify', 'less', 'copy', 'cssmin']);
 
 };
--- js/cb.js
+++ js/cb.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function(){
     /*image array starts*/
-    var imageArrayWeddings = [{title: "kela00"}, {title: "kela01"}, {title: "kela02"}, {title: "kela03"}, {title: "kela04"}, {title: "kela05"}, {title: "kela06"}, {title: "kela07"}, {title: "kela08"}, {title: "kela09"}, {title: "kela10"}, {title: "kela11"}, {title: "kela12"}, {title: "kela13"}];
+    var imageArrayWeddings = [{title: "kela00"}, {title: "kela01"}, {title: "kela02"}, {title: "kela03"}, {title: "kela04"}, {title: "kela05"}, {title: "kela06"}, {title: "kela07"}, {title: "kela08"}, {title: "kela09"}, {title: "kela10"}, {title: "kela11"}, {title: "kela12"}, {title: "kela13"},{title: "kela00"}, {title: "kela01"}, {title: "kela02"}, {title: "kela03"}, {title: "kela04"}, {title: "kela05"}, {title: "kela06"}, {title: "kela07"}, {title: "kela08"}, {title: "kela09"}, {title: "kela10"}, {title: "kela11"}, {title: "kela12"}, {title: "kela13"}, {title: "kela11"}, {title: "kela12"}, {title: "kela13"}];
     var imageArrayEvents = [{title: "baal00"}, {title: "baal01"}, {title: "baal02"}, {title: "baal03"}, {title: "baal04"}, {title: "baal05"}, {title: "baal06"}, {title: "baal07"}, {title: "baal08"}, {title: "baal09"}, {title: "baal10"}, {title: "baal11"}, {title: "baal12"}, {title: "baal13"}];
-    var imageArrayBabies = [{title: "sent00"}, {title: "sent01"}, {title: "sent02"}, {title: "sent03"}, {title: "sent04"}, {title: "sent05"}, {title: "sent06"}, {title: "sent07"}, {title: "sent08"}, {title: "sent09"}, {title: "sent10"}, {title: "sent11"}, {title: "sent12"}, {title: "sent13"}];
+    var imageArrayBabies = [{title: "sent00"}, {title: "sent01"}, {title: "sent02"}, {title: "sent03"}, {title: "sent04"}, {title: "sent05"}, {title: "sent06"}, {title: "sent07"}, {title: "sent08"}, {title: "sent09"}, {title: "sent10"}, {title: "sent11"}, {title: "sent12"}, {title: "sent13"},{title: "sent00"}, {title: "sent01"}, {title: "sent02"}, {title: "sent03"}, {title: "sent04"}, {title: "sent05"}, {title: "sent06"}, {title: "sent07"}, {title: "sent08"}, {title: "sent09"}, {title: "sent10"}];
     var imageArrayProducts = [{title: "marani00"}, {title: "marani01"}, {title: "marani02"}, {title: "marani03"}, {title: "marani04"}, {title: "marani05"}, {title: "marani06"}, {title: "marani07"}, {title: "marani08"}, {title: "marani09"}, {title: "marani10"}, {title: "marani11"}, {title: "marani12"}, {title: "marani13"}];
     var imageArrayInteriors = [{title: "bhaai00"}, {title: "bhaai01"}, {title: "bhaai02"}, {title: "bhaai03"}, {title: "bhaai04"}, {title: "bhaai05"}, {title: "bhaai06"}, {title: "bhaai07"}, {title: "bhaai08"}, {title: "bhaai09"}, {title: "bhaai10"}, {title: "bhaai11"}, {title: "bhaai12"}, {title: "bhaai13"}];
     /*image array ends*/
@@ -31,11 +31,17 @@ jQuery(document).ready(function(){
                 '</li>');
         }
     };
-    var initiateWorSlide = function(){
+    var initiateWorSlide = function(length){
         // Initialize Advanced Galleriffic Gallery
+        var numOfThumbs;
+        if(length){
+            numOfThumbs = length;
+        }else {
+            numOfThumbs = 22;
+        }
         gallery = $('#thumbs').galleriffic({
             delay:                     7500,
-            numThumbs:                 22,
+            numThumbs:                 numOfThumbs,
             preloadAhead:              10,
             enableTopPager:            false,
             enableBottomPager:         false,
@@ -72,7 +78,7 @@ jQuery(document).ready(function(){
         jQuery(".mk_weddings").bind("click", function(){
             gallery.destroy();
             loadWorkGalleryTemplate("weddings", imageArrayWeddings);
-            initiateWorSlide();
+            initiateWorSlide(30);
         });
         jQuery(".mk_events").bind("click", function(){
             gallery.destroy();
@@ -82,7 +88,7 @@ jQuery(document).ready(function(){
         jQuery(".mk_babies").bind("click", function(){
             gallery.destroy();
             loadWorkGalleryTemplate("babies", imageArrayBabies);
-            initiateWorSlide();
+            initiateWorSlide(23);
         });
         jQuery(".mk_products").bind("click", function(){
             gallery.destroy();
@@ -97,7 +103,7 @@ jQuery(document).ready(function(){
         // We only want these styles applied when javascript is enabled
         $('div.work-gallery').css('display', 'flex');
         loadWorkGalleryTemplate("weddings", imageArrayWeddings);
-        initiateWorSlide();
+        initiateWorSlide(30);
 
 
         /**************** Event handlers for custom next / prev page links **********************/
